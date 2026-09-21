f18627d2b284413142d486a3299f2c672ef274df
4fkstztvbs-arch <4fkstztvbs@privaterelay.appleid.com>
2026-09-21 16:23:07 +0200

    Oprav regex produktového NAME

--- scripts/localize-product-names.js
+++ scripts/localize-product-names.js
@@ -18,7 +18,7 @@ function cdata(value) {
 }
 
 function replaceName(block, name) {
-  const re = /^(<SHOPITEM(?:\\s[^>]*)?>\\s*)<NAME>[\\s\\S]*?<\\/NAME>/;
+  const re = /^(<SHOPITEM(?:\s[^>]*)?>\s*)<NAME>[\s\S]*?<\/NAME>/;
   if (!re.test(block)) throw new Error('Expected product-level <NAME> directly under SHOPITEM');
   return block.replace(re, (full, prefix) => prefix + '<NAME>' + cdata(name) + '</NAME>');
 }
